f8538922d501a6df1835802c524d666498594bff
KieranLong11 <14157683@studentmail.ul.ie>
2017-10-24 22:00:51 +0100

    Visitor Pattern, Changes to Windows, Encrypted passwords

--- src/View/LoggedInAsUser.java
+++ src/View/LoggedInAsUser.java
@@ -15,6 +15,8 @@ import View.LogInWindow;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
 public class LoggedInAsUser extends LogInWindow{
 
@@ -67,16 +69,61 @@ public class LoggedInAsUser extends LogInWindow{
 				frame.setVisible(true);
 			}
 		});
-		btnLogOut.setFont(new Font("Arial Black", Font.PLAIN, 11));
-		btnLogOut.setBounds(394, 11, 89, 34);
+		btnLogOut.setFont(new Font("Arial Black", Font.PLAIN, 10));
+		btnLogOut.setBounds(404, 20, 79, 24);
 		frmLoggedUser.getContentPane().add(btnLogOut);
 		
 		JLabel userLabel = new JLabel("Welcome back: " + currentUser.getUsername());
 		userLabel.setFont(new Font("Arial Black", Font.PLAIN, 12));
 		userLabel.setBounds(10, 55, 267, 24);
 		frmLoggedUser.getContentPane().add(userLabel);
+		
+		JButton btnViewStandings = new JButton("View Standings");
+		btnViewStandings.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewStandings.setBounds(355, 178, 117, 33);
+		frmLoggedUser.getContentPane().add(btnViewStandings);
+		
+		JButton btnEditTeam = new JButton("Edit Team");
+		btnEditTeam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEditTeam.setBounds(355, 222, 117, 33);
+		frmLoggedUser.getContentPane().add(btnEditTeam);
+		
+		JButton btnEditTeamName = new JButton("Edit Team Name");
+		btnEditTeamName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEditTeamName.setBounds(355, 266, 117, 33);
+		frmLoggedUser.getContentPane().add(btnEditTeamName);
+		
+		JButton btnPoints = new JButton("View All Points");
+		btnPoints.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnPoints.setBounds(355, 310, 117, 33);
+		frmLoggedUser.getContentPane().add(btnPoints);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(20, 115, 257, 228);
+		frmLoggedUser.getContentPane().add(textPane);
+		
+		JLabel lblYourSquadpoints = new JLabel("Your Squad/Points");
+		lblYourSquadpoints.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblYourSquadpoints.setBounds(20, 90, 117, 24);
+		frmLoggedUser.getContentPane().add(lblYourSquadpoints);
+		
+		JLabel lblWeekNo = new JLabel("Week No: ");
+		lblWeekNo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblWeekNo.setBounds(355, 115, 117, 33);
+		frmLoggedUser.getContentPane().add(lblWeekNo);
 		frmLoggedUser.setBounds(100, 100, 509, 412);
 		frmLoggedUser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
--- src/View/TeamSetUp.java
+++ src/View/TeamSetUp.java
@@ -13,6 +13,8 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JTextPane;
 
 public class TeamSetUp extends CreateAccount{
 
@@ -49,7 +51,7 @@ public class TeamSetUp extends CreateAccount{
 	private void initialize() {
 		frmTeamSetUp = new JFrame();
 		frmTeamSetUp.getContentPane().setBackground(new Color(240, 230, 140));
-		frmTeamSetUp.setBounds(100, 100, 476, 426);
+		frmTeamSetUp.setBounds(100, 100, 509, 427);
 		frmTeamSetUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTeamSetUp.getContentPane().setLayout(null);
 		
@@ -60,7 +62,7 @@ public class TeamSetUp extends CreateAccount{
 		
 		JLabel lblSelectYourTeam = new JLabel("Select Your Team");
 		lblSelectYourTeam.setFont(new Font("Arial Black", Font.BOLD, 12));
-		lblSelectYourTeam.setBounds(31, 80, 145, 30);
+		lblSelectYourTeam.setBounds(20, 77, 145, 30);
 		frmTeamSetUp.getContentPane().add(lblSelectYourTeam);
 		
 		JLabel userLabel = new JLabel("Welcome: " + username);
@@ -72,11 +74,11 @@ public class TeamSetUp extends CreateAccount{
 		
 		JLabel lblTeamName = new JLabel("Team Name: ");
 		lblTeamName.setFont(new Font("Arial Black", Font.PLAIN, 12));
-		lblTeamName.setBounds(31, 121, 87, 24);
+		lblTeamName.setBounds(20, 105, 87, 24);
 		frmTeamSetUp.getContentPane().add(lblTeamName);
 		
 		textField = new JTextField();
-		textField.setBounds(128, 121, 112, 23);
+		textField.setBounds(106, 107, 112, 23);
 		frmTeamSetUp.getContentPane().add(textField);
 		textField.setColumns(10);
 		
@@ -89,9 +91,67 @@ public class TeamSetUp extends CreateAccount{
 			}
 		});
 		btnNewButton.setFont(new Font("Arial Black", Font.PLAIN, 12));
-		btnNewButton.setBounds(0, 364, 89, 23);
+		btnNewButton.setBounds(0, 350, 89, 23);
 		frmTeamSetUp.getContentPane().add(btnNewButton);
+		
+		JList list = new JList();
+		list.setBounds(117, 338, 101, -138);
+		frmTeamSetUp.getContentPane().add(list);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(20, 154, 287, 173);
+		frmTeamSetUp.getContentPane().add(textPane);
+		
+		JLabel lblPickPlayers = new JLabel("Pick Players");
+		lblPickPlayers.setBounds(20, 130, 69, 24);
+		frmTeamSetUp.getContentPane().add(lblPickPlayers);
+		
+		JButton btnConfirmTeam = new JButton("Confirm Team");
+		btnConfirmTeam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnConfirmTeam.setBounds(206, 338, 101, 23);
+		frmTeamSetUp.getContentPane().add(btnConfirmTeam);
+		
+		JButton btnViewGoalkeepers = new JButton("View Goalkeepers");
+		btnViewGoalkeepers.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewGoalkeepers.setBounds(356, 154, 117, 23);
+		frmTeamSetUp.getContentPane().add(btnViewGoalkeepers);
+		
+		JButton btnViewDefenders = new JButton("View Defenders");
+		btnViewDefenders.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewDefenders.setBounds(356, 189, 117, 23);
+		frmTeamSetUp.getContentPane().add(btnViewDefenders);
+		
+		JButton btnViewMidfielders = new JButton("View Midfielders");
+		btnViewMidfielders.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewMidfielders.setBounds(356, 223, 117, 23);
+		frmTeamSetUp.getContentPane().add(btnViewMidfielders);
+		
+		JButton btnViewForwards = new JButton("View Forwards");
+		btnViewForwards.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewForwards.setBounds(356, 257, 117, 23);
+		frmTeamSetUp.getContentPane().add(btnViewForwards);
+		
+		JButton btnViewPlayersPoints = new JButton("Player's Points");
+		btnViewPlayersPoints.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewPlayersPoints.setBounds(356, 291, 117, 23);
+		frmTeamSetUp.getContentPane().add(btnViewPlayersPoints);
 	}
-	
-
 }
